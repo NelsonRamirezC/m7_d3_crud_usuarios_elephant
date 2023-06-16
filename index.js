402278a81b9371f9c030fedb20fa83e5aae1a568
Nelson Ramirez <xhyskoax@gmail.com>
2023-06-16 12:52:37 -0400

    creamos servidor con express.js en archivo app.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import "dotenv/config";
 import db from "./src/db/db.js";
+import app from "./src/app.js";
 
 const main = async () => {
-    let client = await db.connect();
-    let result = await client.query("select * from usuarios;");
-    console.log(result.rows);
+    try {
+        let client = await db.connect();
+        let result = await client.query("select now()");
+        console.log(
+            "Conectado a la base de datos, hora servidor: " + result.rows[0].now
+        );
+        const PORT = process.env.PORT || 3000;
+        let servidor = app.listen(PORT);
+        console.log("Servidor escuchando en http://localhost:3000");
+    } catch (error) {
+        console.log("error al iniciar el servidor: ", error);
+    }
 };
 main();
