c84c1af5a69e3639d0afcc2698cab885f959051d
Nelson Ramirez <xhyskoax@gmail.com>
2023-06-16 12:41:50 -0400

    llevamos la lógica que teniamos en el archivo db.js a el index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import "dotenv/config";
+import db from "./src/db/db.js";
+
+const main = async () => {
+    let client = await db.connect();
+    let result = await client.query("select * from usuarios;");
+    console.log(result.rows);
+};
+main();
--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,4 @@
 import pkg from "pg";
-import "dotenv/config";
 const { Pool } = pkg;
 
 const config = {
@@ -11,30 +10,6 @@ const config = {
     max: process.env.DB_MAX || 20,
 };
 
-let pool;
-let client;
-try {
-    pool = new Pool(config);
-    client = await pool.connect();
-} catch (error) {
-    console.log(error);
-}
+const pool = new Pool(config);
 
-const consulta = async (query) => {
-    try {
-        let result = await client.query(query);
-        console.log("Comando: ", result.command);
-        console.log("Cantidad registros: ", result.rowCount);
-        console.log("Registros:");
-        console.table(result.rows);
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-let query = {
-    text: "SELECT * FROM USUARIOS",
-    values: [],
-};
-
-consulta(query);
+export default pool;
